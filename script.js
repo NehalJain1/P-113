52b9cfc27eac3eef04f2aff07a0c9b7361e1e851
NehalJain1 <90461957+NehalJain1@users.noreply.github.com>
2022-06-22 18:26:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+prediction_label = "";
+prediction_accuracy = "";
 Webcam.set({
     width: 320,
     height: 240,
@@ -20,3 +22,50 @@ Webcam.set({
  function model_loaded() {
      console.log("Model has loaded!");
  }
+
+ function Speak() {
+    //Synth is the variable which is holding the text to speech API
+    var synth = window.speechSynthesis;
+    var speak_data1 = "The prediction is"+prediction_label;
+    var speak_data2 = "with the accuracy"+prediction_accuracy;
+    var utterThis = new SpeechSynthesisUtterance(speak_data1+speak_data2);
+    synth.speak(utterThis) ;
+ }
+
+ function PredictEmotion() {
+   captured_img = document.getElementById("captured_image");
+     classifier.classify(captured_img,gotresults); 
+ }
+
+ function gotresults(error,results) {
+     if(error) {
+         console.error(error);
+     }
+     else {
+    console.log(results);
+    document.getElementById("gesture_name").innerHTML = results[0].label;
+    document.getElementById("gesture_accuracy").innerHTML = results[0].confidence.toFixed(2)*100 + "%";
+    prediction_label = results[0].label ;
+    prediction_accuracy = results[0].confidence.toFixed(2)*100 + "%" ;
+    Speak();
+     //-----------------------------------------------------------------------
+     if (results[0].label == "Thumbs up") {
+        document.getElementById("hand_gesture").innerHTML = "&#128545;" ;
+        }
+        if (results[0].label == "Victory hand") {
+         document.getElementById("hand_gesture").innerHTML = "&#128532;" ;
+         }
+         if (results[0].label == "Thumbs down") {
+             document.getElementById("hand_gesture").innerHTML = "&#128522;" ;
+             }
+         if (results[1].label == "") {
+             document.getElementById("hand_gesture").innerHTML = "&#128545;" ;
+             }
+             if (results[1].label == "Sad") {
+              document.getElementById("hand_gesture").innerHTML = "&#128532;" ;
+              }
+       if (results[1].label == "Happy") {
+                  document.getElementById("hand_gesture").innerHTML = "&#128522;" ;
+                  }
+      }
+  }
